450f81d1b1bea6c55f880bcdb7fa179af9c9ac8e
Júlio Alves <julio619alves@gmail.com>
2021-12-01 02:21:33 -0300

    create usecase

---
+++ usecase/process_transaction/process_transaction_test.go
@@ -0,0 +1,38 @@
+package process_transaction
+
+import (
+	"testing"
+
+	mock_entity "github.com/jknvlvxs/imersao.fullcycle-learning-go/entity/mock"
+
+	"github.com/golang/mock/gomock"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProcessTransactionWhenItsValid(t *testing.T) {
+	input := TransactionDtoInput{
+		ID:        "1",
+		AccountID: "1",
+		Amount:    200,
+	}
+
+	expectedOutput := TransactionDtoOutput{
+		ID:           "1",
+		Status:       "approved",
+		ErrorMessage: "",
+	}
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	repositoryMock := mock_entity.NewMockTransactionRepository(ctrl)
+	repositoryMock.EXPECT().Insert(input.ID, input.AccountID, input.Amount, "approved", "").Return(nil)
+
+	usecase := NewProcessTransaction(repositoryMock)
+	output, err := usecase.Execute(input)
+
+	assert.Nil(t, err)
+	assert.Equal(t, expectedOutput, output)
+
+}
